bd61191629494197650840bdb3d62c3d3768edb8
pubgmmmmmm248-sketch <pubgmmmmmm248@gmail.com>
2026-06-20 15:15:10 +0500

    Create KeyHandler.java

---
+++ src/main/java/com/tntwarner/src/main/java/com/tntwarner/config/src/main/java/com/tntwarner/handler/src/main/java/com/tntwarner/handler/src/main/java/com/tntwarner/handler/KeyHandler.java
@@ -0,0 +1,8 @@
+package com.tntwarner.handler;
+
+import com.tntwarner.gui.ConfigScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.
