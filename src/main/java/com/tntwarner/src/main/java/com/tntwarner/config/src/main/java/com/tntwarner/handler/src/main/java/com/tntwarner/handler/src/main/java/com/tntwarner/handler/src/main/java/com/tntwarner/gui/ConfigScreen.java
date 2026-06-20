43286c32ce86eb8257070486e7ad7ba30ed06d52
pubgmmmmmm248-sketch <pubgmmmmmm248@gmail.com>
2026-06-20 15:15:38 +0500

    Create ConfigScreen.java

---
+++ src/main/java/com/tntwarner/src/main/java/com/tntwarner/config/src/main/java/com/tntwarner/handler/src/main/java/com/tntwarner/handler/src/main/java/com/tntwarner/handler/src/main/java/com/tntwarner/gui/ConfigScreen.java
@@ -0,0 +1,12 @@
+package com.tntwarner.gui;
+
+import com.tntwarner.config.TNTWarnerConfig;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.gui.widget.button.CheckboxButton;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class ConfigScreen extends Screen {
+
+    private final Screen parent;
