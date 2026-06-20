3d6980c05d9cb48b4d62f5b1d70f2e075d29482c
pubgmmmmmm248-sketch <pubgmmmmmm248@gmail.com>
2026-06-20 15:12:47 +0500

    Create TNTWarnerMod.java

---
+++ src/main/java/com/tntwarner/TNTWarnerMod.java
@@ -0,0 +1,14 @@
+package com.tntwarner;
+
+import com.tntwarner.config.TNTWarnerConfig;
+import com.tntwarner.handler.HUDRenderer;
+import com.tntwarner.handler.KeyHandler;
+import com.tntwarner.handler.TNTTracker;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(TNTWa
