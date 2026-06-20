afaadfd98065e30f0c7225f47b853566d8e0d1df
pubgmmmmmm248-sketch <pubgmmmmmm248@gmail.com>
2026-06-20 15:13:24 +0500

    Create TNTWarnerConfig.java

---
+++ src/main/java/com/tntwarner/src/main/java/com/tntwarner/config/TNTWarnerConfig.java
@@ -0,0 +1,13 @@
+package com.tntwarner.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class TNTWarnerConfig {
+    public static final ForgeConfigSpec SPEC;
+    public static final ForgeConfigSpec.IntValue MAX_DISTANCE;
+    public static final ForgeConfigSpec.IntValue MAX_ENTRIES;
+    public static final ForgeConfigSpec.EnumValue<HudPosition> HUD_POSITION;
+    public static final ForgeConfigSpec.BooleanValue SHOW_DISTANCE;
+    public static final ForgeConfigSpec.BooleanValue SHOW_COORDINATES;
+    public static final ForgeConfigSpec.BooleanValue SOUND_ALERT;
+    public static final ForgeConfigSpec.IntValue HUD_SCALE_PERCENT;
